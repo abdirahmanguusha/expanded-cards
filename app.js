fa641bc8ab3022f8fa7f5417ad6a2dd7b3796eaf
abdirahmanguusha <guushaa2013@hotmail.com>
2024-04-06 19:40:59 +0100

    final update of all the files

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const containers = document.querySelectorAll(".container");
-containers.forEach((container)=>{
-    container.addEventListener("click",(e)=>{
-        containers.forEach((cont)=>{
-            cont.classList.remove("expanded");
-        })
-        e.target.parentNode.classList.add("expanded");
-        e.target.parentNode.querySelector("h2").style.opacity = "1";
-
-    })
-
-})
+containers.forEach((container) => {
+  container.addEventListener("click", (e) => {
+    containers.forEach((cont) => {
+      cont.classList.remove("expanded");
+    });
+    e.target.parentNode.classList.add("expanded");
+  });
+});
